bd2d3a02a769eb8c8bb0d5ad8e35255ee2518b95
Jaime <jaimequilez2601@yahoo.es>
2023-05-16 18:40:48 +0200

    inicio blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,32 @@
 import Layout from '../components/layout.js';
+import Post from '../components/post.js';
+import styles from '../styles/grid.module.css';
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
-    <Layout title={'Blog'} description={'Blog'}>
-      <h1>HOLA DESDE Blog</h1>
+    <Layout title={'Blog'} description={'Blog de música, venta de guitarras, consejos'}>
+      <main className="contenedor">
+        <h1 className="heading">Blog</h1>
+        <div className={styles.grid}>
+          {posts?.map(post => {
+            return <Post key={post.id} post={post.attributes} />;
+          })}
+        </div>
+      </main>
     </Layout>
   );
 };
 
 export default Blog;
+
+//Esta informacion se quedara fija en el build, no se regenera con cada visita del usuario
+export async function getStaticProps() {
+  //Esto se ejecuta en el lado del servidor
+  const respuesta = await fetch(`${process.env.API_URL}/posts?populate=imagen`);
+  const { data: posts } = await respuesta.json();
+  return {
+    props: {
+      posts,
+    },
+  };
+}
